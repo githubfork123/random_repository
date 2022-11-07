5104b0e6f2199e210f1688366296b9bd4a877cc1
Eljas <eljas.roellin@gmail.com>
2022-11-07 10:01:16 +0100

    fix for main.py

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ print("this file was created online")
 print("it is quite rainy today")
 print('This change was added after forking')
 print('This change was applepie locally on the forked applepie')
+
+print('suggest to make this line as fix')
